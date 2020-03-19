d263ce73b8e6119c6af1737c279c302f850f9f0a
Padang Basudewa <padangbasudewa4@gmail.com>
2020-03-19 13:21:33 +0700

    Edit TodoReducer

--- src/reducers/TodoReducer.js
+++ src/reducers/TodoReducer.js
@@ -1,3 +1,14 @@
+//if you want to show initial data :)
+//const INITIAL_DATA = [
+// {
+//    id:0
+//    text :'Walk the Dog'
+//},
+//{
+//    id:1
+//    text :'learn Redux',
+//},
+//]
 import { ADD_TODO, REMOVE_TODO, TOGGLE_TODO } from '../actions/actionsTypes' 
 const INITIAL_DATA = []
 const TodoReducer = (state = INITIAL_DATA, action) => {
